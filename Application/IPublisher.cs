6c3ae0b233e01f2baa146015b2a73949ced0c8f6
michaelserupmj <michaelserupmj@gmail.com>
2019-01-30 09:13:40 +0100

    Tilføjet observer interfaces

---
+++ Application/IPublisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    interface IPublisher
+    {
+        void RegisterSubscriber(ISubscriber observer);
+        void RemoveSubscriber(ISubscriber observer);
+        void NotifySubscribers(string message);
+
+    }
+}
